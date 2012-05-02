70da35ccacd06fa472cc4e1878ddfb8a7b8f8f5e
Girah Panopio <girah.panopio@orangeandbronze.com>
2012-05-02 16:01:01 +0800

    first commit

---
+++ grails-app/domain/com/onb/registrationsystem/Teacher.groovy
@@ -0,0 +1,11 @@
+package com.onb.registrationsystem
+
+class Teacher {
+	String firstName
+	String lastName
+
+    static constraints = {
+    	firstName nullable:false, blank:false
+	lastName nullable:false, blank:false
+    }
+}
